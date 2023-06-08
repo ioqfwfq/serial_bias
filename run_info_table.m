920281713e5c45dbe665a719b33b87f42c4d8b81
JZhu <zhujunda0@gmail.com>
2023-06-08 17:28:11 -0500

    Update run_info_table.m
    % add lum of cue for low contrast task condition

--- run_info_table.m
+++ run_info_table.m
@@ -1,24 +1,26 @@
 function info_table = run_info_table
-% fixed bug to correctly note the peak locations
-% 9-19-2022 JZ
-clearvars;
+% add lum of cue for low contrast task condition
+% 20230608 J Zhu
+clearvars
 [file,~] = uigetfile('*.mat',...
     'Select One or More Files', ...
     'MultiSelect', 'on');
 for nf = 1:length(file)
     filename = [file{nf}];
-    [delay, n_class, n_trial, n_cor_trial, PeakLoc1, PeakLoc2, sess_date] = get_sess_info(filename);
-    info_table(nf,:) = [filename, string([delay, n_class, n_trial, n_cor_trial, PeakLoc1, PeakLoc2]), sess_date];
+    [delay, n_class, n_trial, n_cor_trial, PeakLoc1, PeakLoc2, sess_date, lum] = get_sess_info(filename);
+    info_table(nf,:) = [filename, string([delay, n_class, n_trial, n_cor_trial, PeakLoc1, PeakLoc2]), sess_date, lum];
 end
 end
-%% get session information
-function [delay, n_class, n_trial, n_cor_trial, peak_loc1, peak_loc2, sess_date] = get_sess_info(filename)
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% get session information
+function [delay, n_class, n_trial, n_cor_trial, peak_loc1, peak_loc2, sess_date, lum] = get_sess_info(filename)
 load(filename)
 delay = AllData.parameters.delayDuration;
 n_class = length(AllData.ClassStructure);
 n_trial = length(AllData.trials);
 n_cor_trial = length(find([AllData.trials.Statecode]==7));
 sess_date = [num2str(AllData.synctime(1)), num2str(AllData.synctime(2)), num2str(AllData.synctime(3))]; % yyyymmdd
+lum = AllData.parameters.luminance;
 try
     [loc_hist,loc_hist_ind] = ksdensity(AllData.ClassDistribution);
     peak_class = ceil(loc_hist_ind(loc_hist == max(loc_hist)));
@@ -28,7 +30,7 @@ try
     peak_loc1 = str2double(AllData.ClassStructure(peak_class).Notes);
     peak_loc2 = peak_loc1+180;
 catch
-    peak_loc1 = 0;
-    peak_loc2 = 180;
+    peak_loc1 = nan;
+    peak_loc2 = nan;
 end
 end
